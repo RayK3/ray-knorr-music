c609c7610e7ca6cd8a515f4eddd64922e7844cb5
RayK3 <rdk3663@gmail.com>
2024-02-17 17:20:39 -0400

    feat(music): added very basic music page

--- src/routes/musicBase.tsx
+++ src/routes/musicBase.tsx
@@ -1,7 +1,16 @@
+import React from "react";
+
 export const MusicBase = () => {
   return (
-    <div>
-      <h1>I'm a Musician</h1>
+    <div className="relative w-screen h-screen bg-seal-brown">
+      <a href="/" className="absolute z-10 w-16 h-auto top-8 right-8">
+        <img src="/sunglow-logo-1000.png" alt="Sunglow Logo" />
+      </a>
+      <div className="absolute top-32">
+        <p className="px-10 text-xl font-fragment-mono text-sunglow">
+          Coming soon...
+        </p>
+      </div>
     </div>
   );
 };
